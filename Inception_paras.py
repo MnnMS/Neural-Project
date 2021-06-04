04f07b281574407af4fd31357e3c5fffa274a8b5
mennatalah <49452743+mennatalah@users.noreply.github.com>
2021-06-04 21:36:28 +0200

    Create Inception_paras.py

---
+++ Inception_paras.py
@@ -0,0 +1,13 @@
+TRAIN_DIR_A = 'train/train/autistic'
+TRAIN_DIR_N = 'train/train/non_autistic'
+TEST_DATA_DIR = 'test/test'
+TRAIN_DIR = 'train_data.npy'
+TEST_DIR = 'test_data.npy'
+
+IMG_SIZE = 227
+LR = .001
+MODEL_NAME = 'googlenet_oxflowers17'
+TRAIN_VALID_SPLIT = 0
+RGB = 1 # 1 for rgb 0 for grayscale
+EPOCHS = 30
+FORCE_TRAIN = False
